c31e40745eec7b56f9ba584047f621a858d5d7d0
Hendrik Leppkes <h.leppkes@gmail.com>
2019-08-27 03:27:25 +0200

    Can't disable XP on classic

--- modules/xp.lua
+++ modules/xp.lua
@@ -107,7 +107,7 @@ end
 
 function XP:UpdateXP(frame)
 	-- At the level cap or XP is disabled, or the pet is actually a vehicle right now, swap to reputation bar (or hide it)
-	if( UnitLevel(frame.unitOwner) == MAX_PLAYER_LEVEL or IsXPUserDisabled() ) then
+	if( UnitLevel(frame.unitOwner) == MAX_PLAYER_LEVEL ) then
 		frame.xpBar.xp:Hide()
 		return
 	end
